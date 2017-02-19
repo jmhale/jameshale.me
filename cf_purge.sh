0a55c35b230ed9cb4f321af26acb7a9450395263
James Hale <jmhale08@gmail.com>
2017-02-19 12:04:47 -0500

    proper env vars

--- cf_purge.sh
+++ cf_purge.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-curl -X DELETE "https://api.cloudflare.com/client/v4/zones/ <%= ENV['CF_ZONEID_JAMESHALEME'] %>/purge_cache" \
-     -H "X-Auth-Email: <%= ENV['CF_API_EMAIL'] %>" \
-     -H "X-Auth-Key: <%= ENV['CF_API_KEY'] %>" \
+curl -X DELETE "https://api.cloudflare.com/client/v4/zones/${CF_ZONEID_JAMESHALEME}/purge_cache" \
+     -H "X-Auth-Email: ${CF_API_EMAIL}" \
+     -H "X-Auth-Key: ${CF_API_KEY}" \
      -H "Content-Type: application/json" \
      --data '{"purge_everything":true}'
 
